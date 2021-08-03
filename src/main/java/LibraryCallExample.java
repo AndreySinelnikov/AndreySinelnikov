bd64e4b8ec8a03f137a0d9a0a8b85932a7c649a4
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-08-03 16:41:37 +0300

    add calculator library dependency

---
+++ src/main/java/LibraryCallExample.java
@@ -0,0 +1,10 @@
+import com.epam.tat.module4.Calculator;
+
+public class LibraryCallExample {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.sum(1, 1));
+    }
+}
