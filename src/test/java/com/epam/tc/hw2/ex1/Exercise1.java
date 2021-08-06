2b24844480f82c102550a267ba735563506c810b
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-08-06 17:51:16 +0300

    update Exercise1 with working soft assert

--- src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
+++ src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
@@ -3,13 +3,21 @@ package com.epam.tc.hw2.ex1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Exercise1 {
 
     @Test
     public void exercise1() {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\Selenium\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.epam.com");
+        WebDriver webdriver = new ChromeDriver();
+        webdriver.manage().window().maximize();
+        SoftAssert softAssert = new SoftAssert();
+
+        // 1. Open test site by URL
+        webdriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+        // 2. Assert Browser title
+        softAssert.assertEquals("Home Page", webdriver.getTitle());
+        softAssert.assertAll();
     }
 }
