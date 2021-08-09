feea77ee14ad56951599caf33ecc95f042514440
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-08-09 11:28:57 +0300

    refactor Exercise2, update Exercise1

--- src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
+++ src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Exercise1 {
 
@@ -25,7 +29,7 @@ public class Exercise1 {
         // 3. Perform login
         List<WebElement> loginDropdowns = webdriver.findElements(By.className("navbar-right"));
         loginDropdowns.get(0).click();
-        webdriver.findElement(By.id("name")).sendKeys("Roman");
+        webdriver.findElement(By.id("name")).sendKeys("Roman"); // refactor to external source
         webdriver.findElement(By.id("password")).sendKeys("Jdi1234");
         webdriver.findElement(By.id("login-button")).click();
         // 4. Assert Username is loggined
@@ -35,6 +39,25 @@ public class Exercise1 {
         WebElement upperNavBar = webdriver.findElements(By.className("m-l8")).get(0);
         //WebElement home = upperNavBar.findElement(By.xpath("//*[text() = 'HOME']"));
         //softAssert.assertTrue(home.isDisplayed());
+
+        // 8. Assert that there is the iframe with “Frame Button” exist
+        List<WebElement> iFramesWithFrameButton = webdriver
+                .findElements(By.cssSelector("[src='https://jdi-testing.github.io/jdi-light/frame-button.html']"));
+        softAssert.assertFalse(iFramesWithFrameButton.isEmpty());
+        // 9. Switch to the iframe and check that there is “Frame Button” in the iframe
+        webdriver.switchTo().frame(iFramesWithFrameButton.get(0));
+        softAssert.assertFalse(webdriver.findElements(By.id("frame-button")).isEmpty());
+        // 10. Switch to original window back
+        webdriver.switchTo().parentFrame();
+        // 11. Assert that there are 5 items in the Left Section are displayed and they have proper text
+        WebElement leftSidebar = webdriver.findElements(By.cssSelector(".sidebar-menu.left")).get(0);
+        List<WebElement> leftSidebarDirectChildren = new ArrayList<>(leftSidebar.findElements(By.xpath("./*")));
+        softAssert.assertTrue(leftSidebarDirectChildren.stream().allMatch(WebElement::isDisplayed));
+
+        List<String> properTextList = List.of("Home", "Contact form", "Service", "Metals & Colors", "Elements packs");
+        List<String> actualTextList = leftSidebarDirectChildren.stream().map(WebElement::getText)
+                                                                        .collect(Collectors.toList());
+        softAssert.assertEquals(actualTextList, properTextList);
 
         softAssert.assertAll();
     }
