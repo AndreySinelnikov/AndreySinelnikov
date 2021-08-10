769e7794a3aa21577657c23e8a4e6301277da223
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-08-10 21:35:08 +0300

    refactor exercises, test data put into a different class

--- src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
+++ src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
@@ -1,17 +1,20 @@
 package com.epam.tc.hw2.ex1;
 
+import static com.epam.tc.hw2.Utils.getDirectChildren;
+import static com.epam.tc.hw2.Utils.getListOfDirectChildrenTexts;
+import static com.epam.tc.hw2.ex1.Exercise1TestData.EXPECTED_TEXTS_UNDER_IMAGES;
+import static com.epam.tc.hw2.ex1.Exercise1TestData.LEFT_SECTION_EXPECTED_TEXTS;
+import static com.epam.tc.hw2.ex1.Exercise1TestData.TEST_USER_DATA;
+import static com.epam.tc.hw2.ex1.Exercise1TestData.UPPER_NAV_BAR_EXPECTED_TEXTS;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Exercise1 {
 
@@ -20,45 +23,64 @@ public class Exercise1 {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\Selenium\\chromedriver.exe");
         WebDriver webdriver = new ChromeDriver();
         webdriver.manage().window().maximize();
-        SoftAssert softAssert = new SoftAssert();
+        SoftAssertions softly = new SoftAssertions();
 
         // 1. Open test site by URL
         webdriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         // 2. Assert Browser title
-        softAssert.assertEquals("Home Page", webdriver.getTitle());
+        softly.assertThat(webdriver.getTitle())
+                .isEqualTo("Home Page");
         // 3. Perform login
         List<WebElement> loginDropdowns = webdriver.findElements(By.className("navbar-right"));
         loginDropdowns.get(0).click();
-        webdriver.findElement(By.id("name")).sendKeys("Roman"); // refactor to external source
-        webdriver.findElement(By.id("password")).sendKeys("Jdi1234");
+        webdriver.findElement(By.id("name")).sendKeys(TEST_USER_DATA.get("LOGIN"));
+        webdriver.findElement(By.id("password")).sendKeys(TEST_USER_DATA.get("PASSWORD"));
         webdriver.findElement(By.id("login-button")).click();
         // 4. Assert Username is loggined
         WebElement nameLabel = webdriver.findElement(By.xpath("//*[@id=\"user-name\" and not(@class=\"hidden\")]"));
-        softAssert.assertEquals(nameLabel.getText(), "ROMAN IOVLEV");
+        softly.assertThat(nameLabel.getText())
+                .isEqualTo(TEST_USER_DATA.get("DISPLAYED_NAME"));
         // 5. Assert that there are 4 items on the header section are displayed and they have proper texts
         WebElement upperNavBar = webdriver.findElements(By.className("m-l8")).get(0);
-        //WebElement home = upperNavBar.findElement(By.xpath("//*[text() = 'HOME']"));
-        //softAssert.assertTrue(home.isDisplayed());
-
+        softly.assertThat(getDirectChildren(upperNavBar))
+                .hasSize(4)
+                .allMatch(WebElement::isDisplayed);
+        softly.assertThat(getListOfDirectChildrenTexts(upperNavBar))
+                .isEqualTo(UPPER_NAV_BAR_EXPECTED_TEXTS);
+        // 6. Assert that there are 4 images on the Index Page and they are displayed
+        WebElement imageRow = webdriver.findElements(By.cssSelector(".row.clerafix.benefits")).get(0);
+        List<WebElement> images = imageRow.findElements(By.className("benefit-icon"));
+        softly.assertThat(images)
+                .hasSize(4)
+                .allMatch(WebElement::isDisplayed);
+        // 7. Assert that there are 4 texts on the Index Page under icons and they have proper text
+        List<String> textsUnderImages = imageRow.findElements(By.className("benefit-txt")).stream()
+                .map(elem -> elem.getAttribute("innerText"))
+                .collect(Collectors.toList());
+        softly.assertThat(textsUnderImages)
+                .hasSize(4)
+                .isEqualTo(EXPECTED_TEXTS_UNDER_IMAGES);
         // 8. Assert that there is the iframe with “Frame Button” exist
-        List<WebElement> iFramesWithFrameButton = webdriver
+        List<WebElement> iframes = webdriver
                 .findElements(By.cssSelector("[src='https://jdi-testing.github.io/jdi-light/frame-button.html']"));
-        softAssert.assertFalse(iFramesWithFrameButton.isEmpty());
+        softly.assertThat(iframes.isEmpty())
+                .isFalse();
         // 9. Switch to the iframe and check that there is “Frame Button” in the iframe
-        webdriver.switchTo().frame(iFramesWithFrameButton.get(0));
-        softAssert.assertFalse(webdriver.findElements(By.id("frame-button")).isEmpty());
+        webdriver.switchTo().frame(iframes.get(0));
+        softly.assertThat(webdriver.findElements(By.id("frame-button")).isEmpty())
+                .isFalse();
         // 10. Switch to original window back
         webdriver.switchTo().parentFrame();
         // 11. Assert that there are 5 items in the Left Section are displayed and they have proper text
-        WebElement leftSidebar = webdriver.findElements(By.cssSelector(".sidebar-menu.left")).get(0);
-        List<WebElement> leftSidebarDirectChildren = new ArrayList<>(leftSidebar.findElements(By.xpath("./*")));
-        softAssert.assertTrue(leftSidebarDirectChildren.stream().allMatch(WebElement::isDisplayed));
+        WebElement leftSection = webdriver.findElements(By.cssSelector(".sidebar-menu.left")).get(0);
+        softly.assertThat(getDirectChildren(leftSection))
+                .hasSize(5)
+                .allMatch(WebElement::isDisplayed);
+        softly.assertThat(getListOfDirectChildrenTexts(leftSection))
+                .isEqualTo(LEFT_SECTION_EXPECTED_TEXTS);
 
-        List<String> properTextList = List.of("Home", "Contact form", "Service", "Metals & Colors", "Elements packs");
-        List<String> actualTextList = leftSidebarDirectChildren.stream().map(WebElement::getText)
-                                                                        .collect(Collectors.toList());
-        softAssert.assertEquals(actualTextList, properTextList);
-
-        softAssert.assertAll();
+        softly.assertAll();
+        // 12. Close Browser
+        //webdriver.quit();
     }
 }
--- src/test/java/com/epam/tc/hw2/ex2/Exercise2.java
+++ src/test/java/com/epam/tc/hw2/ex2/Exercise2.java
@@ -1,12 +1,13 @@
 package com.epam.tc.hw2.ex2;
 
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,21 +19,23 @@ public class Exercise2 {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\Selenium\\chromedriver.exe");
         WebDriver webdriver = new ChromeDriver();
         webdriver.manage().window().maximize();
-        SoftAssert softAssert = new SoftAssert();
+        SoftAssertions softly = new SoftAssertions();
 
         // 1. Open test site by URL
         webdriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         // 2. Assert Browser title
-        softAssert.assertEquals("Home Page", webdriver.getTitle());
+        softly.assertThat(webdriver.getTitle())
+                .isEqualTo("Home Page");
         // 3. Perform login
         List<WebElement> loginDropdowns = webdriver.findElements(By.className("navbar-right"));
         loginDropdowns.get(0).click();
-        webdriver.findElement(By.id("name")).sendKeys("Roman");  // refactor to external source
+        webdriver.findElement(By.id("name")).sendKeys("Roman"); // refactor to external source
         webdriver.findElement(By.id("password")).sendKeys("Jdi1234");
         webdriver.findElement(By.id("login-button")).click();
         // 4. Assert Username is loggined
-        WebElement nameLabel = webdriver.findElement(By.xpath("//*[@id='user-name' and not(@class= 'hidden')]"));
-        softAssert.assertEquals(nameLabel.getText(), "ROMAN IOVLEV");
+        WebElement nameLabel = webdriver.findElement(By.xpath("//*[@id=\"user-name\" and not(@class=\"hidden\")]"));
+        softly.assertThat(nameLabel.getText())
+                .isEqualTo("ROMAN IOVLEV");
         // 5. Open through the header menu Service -> Different Elements Page
         WebElement upperNavBar = webdriver.findElements(By.className("m-l8")).get(0);
         upperNavBar.findElement(By.className("dropdown-toggle")).click();
@@ -54,12 +57,12 @@ public class Exercise2 {
                 .map(entry -> entry.substring(9))
                 .collect(Collectors.toList());
 
-        softAssert.assertTrue(logEntries.contains("Colors: value changed to Yellow"));
-        softAssert.assertTrue(logEntries.contains("metal: value changed to Selen"));
-        softAssert.assertTrue(logEntries.contains("Wind: condition changed to true"));
-        softAssert.assertTrue(logEntries.contains("Water: condition changed to true"));
+        softly.assertThat(logEntries.contains("Colors: value changed to Yellow")).isTrue();
+        softly.assertThat(logEntries.contains("metal: value changed to Selen")).isTrue();
+        softly.assertThat(logEntries.contains("Wind: condition changed to true")).isTrue();
+        softly.assertThat(logEntries.contains("Water: condition changed to true")).isTrue();
 
-        softAssert.assertAll();
+        softly.assertAll();
         // 10. Close browser
         webdriver.quit();
     }
