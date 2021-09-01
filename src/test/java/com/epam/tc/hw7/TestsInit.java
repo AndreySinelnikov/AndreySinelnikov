70dff3ef0477b18ec01dda190448a6b196e3edc4
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-09-02 00:01:30 +0300

    Add test.properties, TestsInit (setup/teardown), README; Update JdiSite and JdiTest

---
+++ src/test/java/com/epam/tc/hw7/TestsInit.java
@@ -0,0 +1,23 @@
+package com.epam.tc.hw7;
+
+import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
+//import static com.epam.jdi.light.elements.init.PageFactory.initSite;
+import static com.epam.jdi.light.elements.composite.WebPage.openSite;
+
+import com.epam.tc.hw7.site.JdiSite;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public interface TestsInit {
+    @BeforeSuite(alwaysRun = true)
+    static void setUp() {
+        killAllSeleniumDrivers();
+        openSite(JdiSite.class);
+        //initSite(JDISite.class);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    static void teardown() {
+        killAllSeleniumDrivers();
+    }
+}
