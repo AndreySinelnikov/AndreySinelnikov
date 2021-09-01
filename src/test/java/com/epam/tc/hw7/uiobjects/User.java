3f712d12c0138415327c86d5870c0a20aae2d35f
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-09-01 23:18:04 +0300

    Add basic Site/Page classes, User class for login, Steps stub

---
+++ src/test/java/com/epam/tc/hw7/uiobjects/User.java
@@ -0,0 +1,8 @@
+package com.epam.tc.hw7.uiobjects;
+
+import com.epam.jdi.tools.DataClass;
+
+public class User extends DataClass<User> {
+    public String name = "Roman";
+    public String password = "Jdi1234";
+}
