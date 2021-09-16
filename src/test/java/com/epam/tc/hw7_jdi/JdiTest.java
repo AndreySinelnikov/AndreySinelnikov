5586b58fa7736d4bcc1fa49b3a5590bbbfd7e7b9
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-09-16 21:26:22 +0300

    Implement logout and logged username assertion

--- src/test/java/com/epam/tc/hw7_jdi/JdiTest.java
+++ src/test/java/com/epam/tc/hw7_jdi/JdiTest.java
@@ -1,7 +1,10 @@
 package com.epam.tc.hw7_jdi;
 
+import static com.epam.tc.hw7_jdi.HomePage.logout;
+import static com.epam.tc.hw7_jdi.JdiSite.homePage;
 import static com.epam.tc.hw7_jdi.JdiSite.loginAs;
 import static com.epam.tc.hw7_jdi.JdiSite.open;
+import static com.epam.tc.hw7_jdi.JdiTestDataProvider.EXPECTED_USERNAME;
 
 import org.testng.annotations.Test;
 
@@ -11,5 +14,8 @@ public class JdiTest implements TestsInit {
     public void testMetalAndColorsSubmission() {
         open();
         loginAs(User.ROMAN);
+        homePage.userName.assertThat().text(EXPECTED_USERNAME);
+
+        logout();
     }
 }
