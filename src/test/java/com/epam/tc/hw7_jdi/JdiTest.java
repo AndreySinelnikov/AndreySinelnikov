d5551b6536140f1b4935cac544aa20439108e503
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-09-18 14:17:49 +0300

    Move login precondition to BeforeMethod, logout functionality to JdiSite

--- src/test/java/com/epam/tc/hw7_jdi/JdiTest.java
+++ src/test/java/com/epam/tc/hw7_jdi/JdiTest.java
@@ -1,15 +1,10 @@
 package com.epam.tc.hw7_jdi;
 
-import static com.epam.tc.hw7_jdi.site.HomePage.loginAs;
 import static com.epam.tc.hw7_jdi.site.JdiSite.headerMenu;
-import static com.epam.tc.hw7_jdi.site.JdiSite.homePage;
+import static com.epam.tc.hw7_jdi.site.JdiSite.logout;
 import static com.epam.tc.hw7_jdi.site.JdiSite.metalsAndColorsPage;
-import static com.epam.tc.hw7_jdi.site.JdiSite.open;
-import static com.epam.tc.hw7_jdi.JdiTestDataProvider.EXPECTED_USERNAME;
 import static com.epam.tc.hw7_jdi.site.MetalsAndColorsPage.getResult;
-import static com.epam.tc.hw7_jdi.site.MetalsAndColorsPage.logout;
 import static com.epam.tc.hw7_jdi.site.MetalsAndColorsPage.metalsAndColorsForm;
-import static com.epam.tc.hw7_jdi.entities.User.ROMAN;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.epam.tc.hw7_jdi.entities.MetalsAndColorsEntry;
@@ -19,11 +14,6 @@ public class JdiTest implements TestsInit {
 
     @Test(dataProvider = "testDataSet", dataProviderClass = JdiTestDataProvider.class)
     public void testMetalAndColorsSubmission(MetalsAndColorsEntry metalsAndColorsEntry) {
-        // 1. Login on JDI site as User
-        open();
-        loginAs(ROMAN);
-        // Expected: Roman Iovlev is logged in
-        homePage.userName.assertThat().text(EXPECTED_USERNAME);
         // 2. Open Metals & Colors page by Header menu
         headerMenu.select("Metals & Colors");
         // Expected: Metals & Colors page is opened
--- src/test/java/com/epam/tc/hw7_jdi/TestsInit.java
+++ src/test/java/com/epam/tc/hw7_jdi/TestsInit.java
@@ -1,13 +1,28 @@
 package com.epam.tc.hw7_jdi;
 
 import static com.epam.jdi.light.elements.init.PageFactory.initSite;
+import static com.epam.tc.hw7_jdi.JdiTestDataProvider.EXPECTED_USERNAME;
+import static com.epam.tc.hw7_jdi.entities.User.ROMAN;
+import static com.epam.tc.hw7_jdi.site.HomePage.loginAs;
+import static com.epam.tc.hw7_jdi.site.JdiSite.homePage;
+import static com.epam.tc.hw7_jdi.site.JdiSite.open;
 
 import com.epam.tc.hw7_jdi.site.JdiSite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public interface TestsInit {
     @BeforeSuite
-    static void setUp() {
+    static void setUpSuite() {
         initSite(JdiSite.class);
+    }
+
+    @BeforeMethod
+    static void setUpMethod() {
+        // 1. Login on JDI site as User
+        open();
+        loginAs(ROMAN);
+        // Expected: Roman Iovlev is logged in
+        homePage.userName.assertThat().text(EXPECTED_USERNAME);
     }
 }
